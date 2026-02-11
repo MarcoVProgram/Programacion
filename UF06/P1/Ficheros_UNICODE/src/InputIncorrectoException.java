a241fbb519cbeb93937ca7b99f2b9d0515701e36
COMPUS021\daw1 <mvalienter2501@educantabria.es>
2026-02-11 08:56:03 +0100

    Cambios finales añadiendo @Serial a los errores.

--- UF06/P1/Ficheros_UNICODE/src/InputIncorrectoException.java
+++ UF06/P1/Ficheros_UNICODE/src/InputIncorrectoException.java
@@ -1,12 +1,16 @@
+import java.io.Serial;
+
 //Clase exception
 public class InputIncorrectoException extends Exception {
 
+    @Serial
     private static final long serialVersionUID = -707568092472381559L;
     private String inputMistake;
 
     //Constructor con parametros
     //@param inputMistake el input mal introducido que salta el fallo
     public InputIncorrectoException(String inputMistake) {
+
         super("El input mal introducido ha sido: " +  inputMistake);
         this.inputMistake = inputMistake;
     }
@@ -15,9 +19,10 @@ public class InputIncorrectoException extends Exception {
     //@return String, por defecto cuando se llama a system out println
     @Override
     public String toString() {
+
         final StringBuilder sb = new StringBuilder("InputIncorrectoException:\n");
         sb.append("Input Mistake: ").append(inputMistake);
+
         return sb.toString();
     }
 }
-
--- UF06/P2/Ficheros_BINARIO/src/InputIncorrectoException.java
+++ UF06/P2/Ficheros_BINARIO/src/InputIncorrectoException.java
@@ -1,12 +1,16 @@
+import java.io.Serial;
+
 //Clase exception
 public class InputIncorrectoException extends Exception {
 
-    private static final long serialVersionUID = 3088996572802421587L;
+    @Serial
+    private static final long serialVersionUID = -707568092472381559L;
     private String inputMistake;
 
     //Constructor con parametros
     //@param inputMistake el input mal introducido que salta el fallo
     public InputIncorrectoException(String inputMistake) {
+
         super("El input mal introducido ha sido: " +  inputMistake);
         this.inputMistake = inputMistake;
     }
@@ -15,8 +19,10 @@ public class InputIncorrectoException extends Exception {
     //@return String, por defecto cuando se llama a system out println
     @Override
     public String toString() {
+
         final StringBuilder sb = new StringBuilder("InputIncorrectoException:\n");
         sb.append("Input Mistake: ").append(inputMistake);
+
         return sb.toString();
     }
 }
